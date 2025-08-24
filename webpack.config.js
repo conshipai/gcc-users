8262d72acf32d23dcd34c256340718b4aff89ee8
conshipai <thomas.ehler@conship.ai>
2025-08-23 19:25:58 -0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    publicPath: 'auto',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+  },
+  devServer: {
+    port: 3002,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    },
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'users',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App'
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: '^18.2.0' },
+        'react-dom': { singleton: true, requiredVersion: '^18.2.0' }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
